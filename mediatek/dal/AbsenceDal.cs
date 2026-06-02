6fd5e05b3805027867c9ed94978798a40bbf4829
belkaidines77-tech <belkaid.ines77@gmail.com>
2026-06-02 14:50:35 +0200

    Ajout Dal

---
+++ mediatek/dal/AbsenceDal.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using mediatek.bddmanager;
+using mediatek.Models;
+
+namespace mediatek.dal
+{
+    public class AbsenceDal
+    {
+        private BddManager _bddManager;
+
+        public AbsenceDal()
+        {
+            _bddManager = BddManager.GetInstance();
+        }
+
+        public List<absence> GetAbsencesByPersonnel(int idpersonnel)
+        {
+            List<absence> absences = new List<absence>();
+            string query = "SELECT idpersonnel, datedebut, datefin, idmotif FROM absence WHERE idpersonnel = @idpersonnel ORDER BY datedebut DESC";
+
+            MySqlConnection conn = _bddManager.GetConnection();
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@idpersonnel", idpersonnel);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                absence a = new absence();
+                a.idpersonnel = reader.GetInt32("idpersonnel");
+                a.datedebut = reader.GetDateTime("datedebut");
+                a.datefin = reader.GetDateTime("datefin");
+                a.idmotif = reader.GetInt32("idmotif");
+                absences.Add(a);
+            }
+            reader.Close();
+            conn.Close();
+            return absences;
+        }
+
+        // Vérifie si une absence chevauche une autre (pour ajout ou modification)
+        public bool IsChevauchement(int idpersonnel, DateTime datedebut, DateTime datefin, DateTime? ancienneDebut = null)
+        {
+            string query = @"
+                SELECT COUNT(*) FROM absence 
+                WHERE idpersonnel = @idpersonnel 
+                AND datedebut < @datefin 
+                AND datefin > @datedebut";
+
+            if (ancienneDebut.HasValue)
+            {
+                query += " AND datedebut != @ancienneDebut";
+            }
+
+            MySqlConnection conn = _bddManager.GetConnection();
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@idpersonnel", idpersonnel);
+            cmd.Parameters.AddWithValue("@datedebut", datedebut);
+            cmd.Parameters.AddWithValue("@datefin", datefin);
+            if (ancienneDebut.HasValue)
+                cmd.Parameters.AddWithValue("@ancienneDebut", ancienneDebut.Value);
+
+            int count = Convert.ToInt32(cmd.ExecuteScalar());
+            conn.Close();
+            return count > 0;
+        }
+
+        public string AddAbsence(absence a)
+        {
+            if (IsChevauchement(a.idpersonnel, a.datedebut, a.datefin))
+                return "Erreur : chevauchement avec une absence existante";
+
+            string query = @"
+                INSERT INTO absence (idpersonnel, datedebut, datefin, idmotif) 
+                VALUES (@idpersonnel, @datedebut, @datefin, @idmotif)";
+
+            MySqlConnection conn = _bddManager.GetConnection();
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@idpersonnel", a.idpersonnel);
+            cmd.Parameters.AddWithValue("@datedebut", a.datedebut);
+            cmd.Parameters.AddWithValue("@datefin", a.datefin);
+            cmd.Parameters.AddWithValue("@idmotif", a.idmotif);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            return "OK";
+        }
+
+        public string UpdateAbsence(absence a, DateTime ancienneDebut)
+        {
+            if (IsChevauchement(a.idpersonnel, a.datedebut, a.datefin, ancienneDebut))
+                return "Erreur : chevauchement avec une absence existante";
+
+            string query = @"
+                UPDATE absence 
+                SET datedebut = @datedebut, datefin = @datefin, idmotif = @idmotif 
+                WHERE idpersonnel = @idpersonnel AND datedebut = @ancienneDebut";
+
+            MySqlConnection conn = _bddManager.GetConnection();
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@datedebut", a.datedebut);
+            cmd.Parameters.AddWithValue("@datefin", a.datefin);
+            cmd.Parameters.AddWithValue("@idmotif", a.idmotif);
+            cmd.Parameters.AddWithValue("@idpersonnel", a.idpersonnel);
+            cmd.Parameters.AddWithValue("@ancienneDebut", ancienneDebut);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            return "OK";
+        }
+
+        public int DeleteAbsence(int idpersonnel, DateTime datedebut)
+        {
+            string query = "DELETE FROM absence WHERE idpersonnel = @idpersonnel AND datedebut = @datedebut";
+            MySqlConnection conn = _bddManager.GetConnection();
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@idpersonnel", idpersonnel);
+            cmd.Parameters.AddWithValue("@datedebut", datedebut);
+            int result = cmd.ExecuteNonQuery();
+            conn.Close();
+            return result;
+        }
+    }
+}
---
+++ mediatek/dal/personneldal.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using mediatek.bddmanager;
+using mediatek.Models;
+
+namespace mediatek.dal
+{
+    public class personneldal
+    {
+        private BddManager bddManager;
+
+        public personneldal()
+        {
+            bddManager = BddManager.GetInstance();
+        }
+
+        public List<personnel> GetAllPersonnels()
+        {
+            List<personnel> personnels = new List<personnel>();
+            string query = "SELECT idpersonnel, nom, prenom, tel, mail, idservice FROM personnel";
+
+            MySqlConnection conn = bddManager.GetConnection();
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                personnel p = new personnel();
+                p.idpersonnel = reader.GetInt32("idpersonnel");
+                p.nom = reader.GetString("nom");
+                p.prenom = reader.GetString("prenom");
+                p.tel = reader.IsDBNull(reader.GetOrdinal("tel")) ? "" : reader.GetString("tel");
+                p.mail = reader.IsDBNull(reader.GetOrdinal("mail")) ? "" : reader.GetString("mail");
+                p.idservice = reader.GetInt32("idservice");
+                personnels.Add(p);
+            }
+            reader.Close();
+            conn.Close();
+            return personnels;
+        }
+
+        public int AddPersonnel(personnel p)
+        {
+            string query = "INSERT INTO personnel (nom, prenom, tel, mail, idservice) VALUES ('" + p.nom + "', '" + p.prenom + "', '" + p.tel + "', '" + p.mail + "', " + p.idservice + ")";
+            MySqlConnection conn = bddManager.GetConnection();
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            int result = cmd.ExecuteNonQuery();
+            conn.Close();
+            return result;
+        }
+
+        public int UpdatePersonnel(personnel p)
+        {
+            string query = "UPDATE personnel SET nom='" + p.nom + "', prenom='" + p.prenom + "', tel='" + p.tel + "', mail='" + p.mail + "', idservice=" + p.idservice + " WHERE idpersonnel=" + p.idpersonnel;
+            MySqlConnection conn = bddManager.GetConnection();
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            int result = cmd.ExecuteNonQuery();
+            conn.Close();
+            return result;
+        }
+
+        public int DeletePersonnel(int id)
+        {
+            string query = "DELETE FROM personnel WHERE idpersonnel=" + id;
+            MySqlConnection conn = bddManager.GetConnection();
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            int result = cmd.ExecuteNonQuery();
+            conn.Close();
+            return result;
+        }
+    }
+}
