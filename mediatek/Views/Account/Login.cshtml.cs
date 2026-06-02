16b06c81379c0b50ff87d9f218de5a56332a165c
belkaidines77-tech <belkaid.ines77@gmail.com>
2026-06-02 14:49:50 +0200

    Ajout des vues

---
+++ mediatek/Views/Account/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mediatek.Views.Account
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
