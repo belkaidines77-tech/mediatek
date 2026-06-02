a87b0cdfa451860efe5095e203c40ed641a3f659
belkaidines77-tech <belkaid.ines77@gmail.com>
2026-06-02 14:38:32 +0200

    connexion à la base de donnée

---
+++ mediatek/bddmanager/BddManager.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace mediatek.bddmanager
+{
+    public class BddManager
+    {
+        private static BddManager instance;
+        private string connectionString;
+        private MySqlConnection connection;
+
+        private BddManager()
+        {
+            connectionString = "Server=localhost;Database=mediatek;Uid=mediatek;Pwd=mediatek86;";
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public static BddManager GetInstance()
+        {
+            if (instance == null)
+                instance = new BddManager();
+            return instance;
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
